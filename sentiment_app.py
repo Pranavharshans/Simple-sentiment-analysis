2e78d7853231c37f60ec5d07b6023588a46f6534
pranavharshans <pranavharshan.s2021cseb@sece.ac.in>
2023-09-12 15:02:43 +0530

    first commit

---
+++ sentiment_app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from textblob import TextBlob
+from dataclasses import dataclass
+
+
+
+
+@dataclass
+class Mood:
+    emoji: str
+    sentiment: float
+
+
+
+
+def get_mood(input_text: str, *, threshold: float) -> Mood:
+    sentiment: float = TextBlob(input_text).sentiment.polarity
+
+    friendly_threshold: float = threshold
+    hostile_threshold: float = -threshold
+
+    if sentiment >= friendly_threshold:
+        return Mood('😃 Positive', sentiment)
+    elif sentiment <= hostile_threshold:
+        return Mood('😠 Negative', sentiment)
+    else:
+        return Mood('😐 Neutral', sentiment)
+
+
+
+def main():
+    st.title("Sentiment Analysis Web App")
+    st.write("Enter text and click the 'Analyze' button to analyze")
+
+    text = st.text_area("Enter your text here:")
+    threshold = 0.3  
+
+
+
+    if st.button("Analyze"):
+        if text:
+            mood = get_mood(text, threshold=threshold)
+            st.subheader("Sentiment Analysis Result")
+            st.write(f'Sentiment: {mood.emoji} ({mood.sentiment})')
+
+
+
+if __name__ == '__main__':
+    main()
